150e5e4c9a183943f718ade53f28a9cad3d28362
saraSL <sara.lundqviist@gmail.com>
2020-10-12 09:28:10 +0200

    divide data into train and valid

--- Version1.py
+++ Version1.py
@@ -3,6 +3,7 @@
 
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow.keras import datasets, layers, models
@@ -13,44 +14,52 @@ testset = pd.read_csv("nn-assignment/testset.csv")
 trainset = pd.read_csv("nn-assignment/trainset.csv")
 
 # check that loded correctly
-print(f'Head of amplesubmission: \n', samplesubmission.head())
+print(f'Head of samplesubmission: \n', samplesubmission.head())
 print(f'Testset dim: ', testset.shape)
 print(f'Trainset dim: ', trainset.shape)
 print(f'Head of trainset: \n', trainset.head())
 
-# extract labels from traningset
-labels = np.array(trainset['label'])
+#split traningset into traning and validationset
+trainset = trainset.sample(frac=1) # shuffle data
+train, valid = train_test_split(trainset, test_size=0.15)
+print(train.head)
+print(valid.head)
 
-# extract images from traningset & reshape
-images = np.array(trainset.drop(['label'], axis=1))
-images = np.reshape(images, (28000, 28, 28))
-# print(images[0,:,:])
-# print(images[0,:,:].shape)
+# extract labels from traningset
+train_labels = np.array(train['label'])
+valid_labels = np.array(valid['label'])
+
+# extract pixelvalues from trainset and validationset & reshape to 28x28 arrays
+train_images = np.array(train.drop(['label'], axis=1))
+train_images = np.reshape(train_images, (23800, 28, 28))
+valid_images = np.array(valid.drop(['label'], axis=1))
+valid_images = np.reshape(valid_images, (4200, 28, 28))
+#print(images[0,:,:])
+#print(images[0,:,:].shape)
 
 # Normalize pixel values to be between 0 and 1
-images = images / 255
+#train_images = train_images / 255
+#valid_images = valid_images / 255
 
-# plot first 25 images to check reshaping
+
+# plot first 25 train_images to check reshaping
 plt.figure(figsize=(28, 28))
 for i in range(25):
     plt.subplot(5, 5, i + 1)
-    plt.imshow(images[i])
+    plt.imshow(train_images[i])
+    plt.xlabel(train_labels[i])
 plt.show()
-print(f'First 25 labels: ', labels[0:25])
+print(f'First 25 labels: ', train_labels[0:25])
+
+#create convolutional base
+
+
+#add dense layers on top
+
+
+#compile and train model
 
 
 
-# #split trainset into traning - and validationset (70/30)
-# trainset['split'] = np.random.randn(trainset.shape[0], 1)
-# print(trainset['split'].head)
-# msk = np.random.rand(len(trainset)) <= 0.7
-# print(msk[1])
-# train = trainset[msk]
-# valid = trainset[~msk]
-# print(train.head)
-# print(train.shape)
-# print(valid.shape)
-# print(type(train))
 
 
-# class_names = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
