24e034ac455e580df8c6debc119b4762f749c7a4
saraSL <sara.lundqviist@gmail.com>
2020-10-12 10:09:42 +0200

    add layers

--- Version1.py
+++ Version1.py
@@ -6,7 +6,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 import tensorflow as tf
-from tensorflow.keras import datasets, layers, models
+from tensorflow.keras import datasets, layers, models, losses
 
 # load data
 samplesubmission = pd.read_csv("nn-assignment/samplesubmission.csv")
@@ -52,14 +52,42 @@ plt.show()
 print(f'First 25 labels: ', train_labels[0:25])
 
 #create convolutional base
-
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
 
 #add dense layers on top
+model.add(layers.Flatten())
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(10))
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+history = model.fit(train_images, train_labels, epochs=10,
+                    validation_data=(test_images, test_labels))
+
+plt.plot(history.history['accuracy'], label='accuracy')
+plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.ylim([0.5, 1])
+plt.legend(loc='lower right')
+
+test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+
+
+print(test_acc)
+
 
 
 #compile and train model
 
 
-
+#The size of the output layer is equal to the number of classes.
 
 
