33eac56809974b03177d5de3068313b36cd903b1
saraSL <sara.lundqviist@gmail.com>
2020-10-12 14:24:47 +0200

    accuracy: 0.9483

--- Version1.py
+++ Version1.py
@@ -19,8 +19,8 @@ print(f'Testset dim: ', testset.shape)
 print(f'Trainset dim: ', trainset.shape)
 print(f'Head of trainset: \n', trainset.head())
 
-#split traningset into traning and validationset
-trainset = trainset.sample(frac=1) # shuffle data
+# split traningset into traning and validationset
+trainset = trainset.sample(frac=1)  # shuffle data
 train, valid = train_test_split(trainset, test_size=0.15)
 print(train.head)
 print(valid.head)
@@ -34,64 +34,69 @@ train_images = np.array(train.drop(['label'], axis=1))
 train_images = np.reshape(train_images, (23800, 28, 28))
 valid_images = np.array(valid.drop(['label'], axis=1))
 valid_images = np.reshape(valid_images, (4200, 28, 28))
-#print(images[0,:,:])
-#print(images[0,:,:].shape)
+# print(images[0,:,:])
+# print(images[0,:,:].shape)
 
 # Normalize pixel values to be between 0 and 1
 train_images = train_images / 255
 valid_images = valid_images / 255
 
-
 # plot first 25 train_images to check reshaping
 plt.figure(figsize=(28, 28))
 for i in range(25):
     plt.subplot(5, 5, i + 1)
-    plt.imshow(train_images[i])
+    plt.imshow(train_images[i], cmap=plt.cm.gray)
     plt.xlabel(train_labels[i])
 plt.show()
 print(f'First 25 labels: ', train_labels[0:25])
 
-#create convolutional base
-model = models.Sequential()
-model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-#Typically, the final activation function is a softmax, which ensures that the output values sum up to 1.
-
-#add dense layers on top
-model.add(layers.Flatten())
-model.add(layers.Dense(64, activation='softmax'))
-model.add(layers.Dense(10))
-
 train_images = np.reshape(train_images, (23800, 28, 28, 1))
 valid_images = np.reshape(valid_images, (4200, 28, 28, 1))
 
-model.compile(loss = 'sparse_categorical_crossentropy', optimizer = 'sgd', metrics = ['accuracy'])
-history = model.fit(train_images, train_labels, batch_size = 32, epochs = 5, validation_data = (valid_images, valid_labels))
-        # X, y − It is a tuple to evaluate your data.
-        # epochs − no of times the model is needed to be evaluated during training.
-        # batch_size − training instances.
+# create convolutional base
+# model = models.Sequential()
+# model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
+# model.add(layers.MaxPooling2D((2, 2)))
+# model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+# model.add(layers.MaxPooling2D((2, 2)))
+# model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+# #Typically, the final activation function is a softmax, which ensures that the output values sum up to 1.
+# #add dense layers on top
+# model.add(layers.Flatten())
+# model.add(layers.Dense(64, activation='relu'))
+# model.add(layers.Dense(10))     #The size of the output layer is equal to the number of classes.
+#
+# model.compile(loss = 'sparse_categorical_crossentropy', optimizer = 'sgd', metrics = ['accuracy'])
+# history = model.fit(train_images, train_labels, batch_size = 32, epochs = 5, validation_data = (valid_images, valid_labels))
+#         # X, y − It is a tuple to evaluate your data.
+#         # epochs − no of times the model is needed to be evaluated during training.
+#         # batch_size − training instances.
 
 
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))  #
+# model.add(layers.MaxPool2D(pool_size=(2, 2))          # Downsamples the input
+model.add(layers.Flatten())
+model.add(layers.Dense(32, activation='relu'))
+model.add(layers.Dropout(0.2))  # helps prevent overfitting
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dropout(0.2))
+model.add(layers.Dense(10, activation='softmax'))
+
+# compile and train model
+model.compile(loss='sparse_categorical_crossentropy', optimizer='RMSprop', metrics=['accuracy'])
+history = model.fit(train_images, train_labels, batch_size=32, epochs=5, validation_data=(valid_images, valid_labels))
+
+# evaluate the model
 plt.plot(history.history['accuracy'], label='accuracy')
-plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+plt.plot(history.history['val_accuracy'], label='val_accuracy')
 plt.xlabel('Epoch')
 plt.ylabel('Accuracy')
 plt.ylim([0.5, 1])
 plt.legend(loc='lower right')
 plt.show()
 
-test_loss, test_acc = model.evaluate(valid_images,  valid_labels, verbose=2)
+test_loss, test_acc = model.evaluate(valid_images, valid_labels, verbose=2)
 
-print(test_acc)
-
-
-
-#compile and train model
-
-
-#The size of the output layer is equal to the number of classes.
-
+print(f'Test accuracy: ', test_acc)
 
